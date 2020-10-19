98d2cca41d29eff12d4218c481396fa714f77348
Thiago <henriquesilva322@outlook.com>
2020-10-18 23:32:55 -0300

    implemento API-GET

--- DesafioTeste2/desafio2/src/pages/filmes/index.tsx
+++ DesafioTeste2/desafio2/src/pages/filmes/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../../componentes/header/index';
 import Footer from '../../componentes/footer/index';
 import './style.css';
 
 function Filmes()
 {
+    const[filmes, setFilmes] = useState([]);
+    const [filme,setFilme] = useState('');
+
+    useEffect(()=>{
+        listarFilmes();
+    },[]);
+
+    const listarFilmes = ()=>{
+        fetch('http://localhost:5000/api/filmes',{
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(dados =>{
+            setFilmes(dados);
+        })
+        .catch(err => console.error(err));
+    }
+
+   
+
     return(
         <div>
             <Header descricao="Venha conhecer nossos filmes"/>
@@ -18,10 +38,17 @@ function Filmes()
                         </tr>
                     </thead>
                     <tbody>
-                        <td>info</td>
-                        <td>info2</td>
+                        {filmes.map((item:any)=>
+                            <tr key={item.idFilme}>
+                                <td>{item.titulo}</td>
+                                <td>{item.idGeneroNavigation.nome}</td>
+                            </tr>
+                        )}
+
+                       
                     </tbody>
                 </table>
+                <p ></p>
             </div>
             <Footer/>
         </div>
